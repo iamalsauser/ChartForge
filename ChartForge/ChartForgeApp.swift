1569cb1ba391913c28418aa6b4732e65df05d360
iamalsauser <parthsinhv4@gmail.com>
2025-07-18 14:48:36 +0530

    Initial Commit

---
+++ ChartForge/ChartForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartForgeApp.swift
+//  ChartForge
+//
+//  Created by Parth Sinh on 18/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ChartForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChartForgeTests/ChartForgeTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChartForgeTests.swift
+//  ChartForgeTests
+//
+//  Created by Parth Sinh on 18/07/25.
+//
+
+import Testing
+
+struct ChartForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
